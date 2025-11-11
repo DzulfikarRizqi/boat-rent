396c01796bdeef163c04fe169d0293bf1a0d4028
rozangangsar <rozangangsar01@gmail.com>
2025-11-12 00:15:09 +0700

    update tampilan cek pesanan

---
+++ Kapalin/CobaHW7/RupiahConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CobaHW7
+{
+    public class RupiahConverter : IValueConverter
+    {
+        private static readonly CultureInfo Id = new("id-ID");
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IFormattable f) return string.Format(Id, "Rp {0:N0}", f);
+            return "Rp 0";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
