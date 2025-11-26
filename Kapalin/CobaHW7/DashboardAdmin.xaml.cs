270f1118e9927306b5ac8cbc705d3cdd70f7ee40
hammampriyandono <hammam.priyandono@gmail.com>
2025-11-26 13:43:30 +0700

    edit tombol logout admin

--- Kapalin/CobaHW7/DashboardAdmin.xaml.cs
+++ Kapalin/CobaHW7/DashboardAdmin.xaml.cs
@@ -1,6 +1,7 @@
 using CobaHW7.Class;
 using CobaHW7.ViewModels;
-//using CobaHW7.Data;
+using CobaHW7.Services;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +22,39 @@ namespace CobaHW7
             if (!BoatsDataGrid.IsMouseOver)
             {
                 BoatsDataGrid.UnselectAll();
+            }
+        }
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var button = sender as Button;
+                if (button != null)
+                    button.IsEnabled = false;
+
+                try
+                {
+                    var signOutTask = SupabaseService.Client.Auth.SignOut();
+                }
+                catch
+                {
+                }
+
+                MainWindow loginWindow = new MainWindow();
+                loginWindow.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                var errorAlert = new AlertWindow("Logout",
+                    "Anda akan diarahkan ke halaman login.",
+                    AlertWindow.AlertType.Info);
+                errorAlert.ShowDialog();
+
+                MainWindow loginWindow = new MainWindow();
+                loginWindow.Show();
+                this.Close();
             }
         }
     }
